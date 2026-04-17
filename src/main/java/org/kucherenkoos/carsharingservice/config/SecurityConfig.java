3fac6533c8323d5a8ec4c7ef3fde545edcf5d669
Kucherenko Oleksandr <116387244+KucherenkoOS@users.noreply.github.com>
2026-04-17 13:27:51 +0300

    chore: ci.yml, SecurityConfig, PaymentServiceImpl, README.md(#7)

--- src/main/java/org/kucherenkoos/carsharingservice/config/SecurityConfig.java
+++ src/main/java/org/kucherenkoos/carsharingservice/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
                                 "/auth/register",
                                 "/auth/login",
                                 "/cars",
+                                "/cars/**",
                                 "/swagger-ui.html",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
